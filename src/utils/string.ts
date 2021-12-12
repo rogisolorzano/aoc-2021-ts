5e12058d8362d9674d89c3035893327582c5f005
Rogi Solorzano <rogi.solorzano@gmail.com>
2021-12-12 06:54:50 -0600

    Day 12!

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -4,3 +4,5 @@
  * @param str
  */
 export const capitalize = (str: string): string => str.charAt(0).toUpperCase() + str.slice(1);
+
+export const isLowerCase = (str: string) => str === str.toLowerCase() && str !== str.toUpperCase();
