46ca5d2cbb5c1746428475b35cb986117927ba8c
Rogi Solorzano <rogi.solorzano@gmail.com>
2021-12-20 03:53:16 -0600

    Day 18 - snail math!

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -4,3 +4,5 @@ export const count = <T>(a: T[], predicate: (v: T) => boolean) =>
   a.reduce((count, n) => predicate(n) ? count + 1 : count, 0);
 
 export const naturalNumbersSummation = (n: number) => n * (n + 1) / 2;
+
+export const isNumber = (n: unknown): n is number => typeof n === 'number';
