fd2cab821d1dd614fd7b5e1a75b8b75f4dceda43
Rogi Solorzano <rogi.solorzano@artisantechgroup.com>
2021-12-27 16:29:10 -0600

    Updates

--- src/day-07/index.ts
+++ src/day-07/index.ts
@@ -6,32 +6,14 @@ const getTotalCost = (currentPosition: number, positions: number[], costFn: Cost
   positions.reduce((total, p) => total + costFn(Math.abs(currentPosition - p)), 0)
 
 /**
- * Loop through all possible positions that the crabs can move to, starting from the
- * min crab position to the max crab position. At each step, we calculate the total
- * fuel that would be used for each crab using the provided cost function. Stops
- * when cost starts sloping up.
+ * Search through possible positions that the crabs can move to. At each step, we
+ * calculate the total fuel that would be used for each crab using the provided
+ * cost function. Uses a binary search approach to cut time complexity down to
+ * O(n log k). Decides to go either left or right depending on what direction is
+ * sloping down. This approach finishes in 9 iterations vs my initial solution
+ * which takes 478 iterations.
  */
-const findOptimalAlignmentSolution1 = (positions: number[], costFn: CostFn) => {
-  let currentCost = Infinity;
-  let previousCost = Infinity;
-  let currentPosition = Math.min(...positions);
-  let maxPosition = Math.max(...positions);
-
-  while (currentCost <= previousCost && currentPosition < maxPosition) {
-    previousCost = currentCost;
-    currentCost = getTotalCost(currentPosition, positions, costFn);
-    currentPosition++;
-  }
-
-  return previousCost;
-}
-
-/**
- * Same idea as solution 1 but uses a binary search approach to cut time complexity down to O(n log k).
- *
- * This approach finishes in 9 iterations vs my initial solution which takes 478 iterations.
- */
-const findOptimalAlignmentSolution2 = (positions: number[], costFn: CostFn) => {
+const findOptimalAlignmentSolution = (positions: number[], costFn: CostFn) => {
   let currentCost = Infinity;
   let minPosition = Math.min(...positions);
   let maxPosition = Math.max(...positions);
@@ -65,13 +47,8 @@ async function main() {
   const lines = (await getAllLines(__dirname, 'input.txt'));
   const crabPositions = lines[0].split(',').map(n => Number(n));
 
-  // Initial solution
-  console.log("Part 1", findOptimalAlignmentSolution1(crabPositions, simpleCostFn));
-  console.log("Part 2", findOptimalAlignmentSolution1(crabPositions, naturalNumbersSummationCostFn));
-
-  // Binary search approach
-  console.log("Part 1", findOptimalAlignmentSolution2(crabPositions, simpleCostFn));
-  console.log("Part 2", findOptimalAlignmentSolution2(crabPositions, naturalNumbersSummationCostFn));
+  console.log("Part 1", findOptimalAlignmentSolution(crabPositions, simpleCostFn));
+  console.log("Part 2", findOptimalAlignmentSolution(crabPositions, naturalNumbersSummationCostFn));
 }
 
 main();
